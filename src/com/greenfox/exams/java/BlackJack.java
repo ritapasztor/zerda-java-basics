4a9b3b3e9129bc309e710023c07cb3877f6429ff
Rita Pasztor <pasztorrita@gmail.com>
2016-12-05 09:46:46 +0100

    add classes: Deck, Card, Player, BlackJack

---
+++ src/com/greenfox/exams/java/BlackJack.java
@@ -0,0 +1,11 @@
+package com.greenfox.exams.java;
+
+import javax.swing.*;
+
+/**
+ * Created by Rita on 2016.12.05..
+ */
+public class BlackJack extends JFrame{
+
+
+}
---
+++ src/com/greenfox/exams/java/Deck.java
@@ -0,0 +1,9 @@
+package com.greenfox.exams.java;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Rita on 2016.12.05..
+ */
+public class Deck extends ArrayList<Card> {
+}
